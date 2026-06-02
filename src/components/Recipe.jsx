928adda47c72aac64357f41a898ac1109c81a9a9
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-02 17:11:27 +0100

    all tests passing for Recipe

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,6 +2,8 @@ const Recipe = (props) => {
     return (
         <>
             <h1>{props.name}</h1>
+            <p data-testid="type">{props.type}</p>
+            <p data-testid="duration">{props.duration}</p>
         </>
     )
 }
--- tests/Recipe.test.jsx
+++ tests/Recipe.test.jsx
@@ -1,7 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Recipe from "../src/components/Recipe";
 
+// <Recipe title="Finnish cinnamon buns" type="dessert" duration={60} />
+
+
 test("renders the correct title", () => {
     render(<Recipe name="Finnish cinnamon buns"/>);
     expect(screen.getByRole("heading")).toHaveTextContent("Finnish cinnamon buns");
-})
+});
+
+test("renders the correct type", () => {
+    render(<Recipe type="dessert"/>);
+    expect(screen.getByTestId("type")).toHaveTextContent("dessert");
+});
+
+test("renders the correct duration", () => {
+    render(<Recipe duration={60}/>);
+    expect(screen.getByTestId("duration")).toHaveTextContent(60);
+});
