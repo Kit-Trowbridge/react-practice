affb7404d0bae6bbe0fbd0d50a3ebc9aa0fb43c9
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-03 16:44:31 +0100

    second counter test passing

--- tests/Counter.test.jsx
+++ tests/Counter.test.jsx
@@ -6,3 +6,13 @@ test("renders with initial setup value of 0", () => {
     render(<Counter />);
     expect(screen.getByRole("heading")).toHaveTextContent(0);
 });
+
+test("renders incremented count (3) when button is pressed three times", async () => {
+    render(<Counter />);
+
+    await userEvent.click(screen.getByText("Increment the counter"));
+    await userEvent.click(screen.getByText("Increment the counter"));
+    await userEvent.click(screen.getByText("Increment the counter"));
+
+    expect(screen.getByRole("heading")).toHaveTextContent(3);
+});
