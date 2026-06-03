b36ddb33e2794c89232b31bf4d36d2e05de070b4
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-03 14:43:48 +0100

    attempt to use useState in component file

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import Profile from "./components/Profile";
 import Product from "./components/Product";
 import TypeLogger from "./components/TypeLogger";
 import ClickListener from "./components/ClickListener";
+import Counter from "./components/Counter";
 import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <TypeLogger />
-      <Hello name="World" />
-      <img className="logo" src={makersLogo}></img> <br />
-      <ClickListener />
-      <Profile 
+      {/* <TypeLogger /> <br />
+      <ClickListener /> */}
+      {/* <Profile 
         name="Quackie Makers"
         job="Makers' favourite rubber duck"
         birthdate="2013"
-      />
-      <Product 
+      /> */}
+      {/* <Product 
         name="Air Fryer K2000"
         description="The best air fryer to fry all things, even Mars bars"
         price={2000}
-      />
+      /> */}
+      <Counter />
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,14 @@
-const Counter = () => {
+import { useState } from "react"
+const Counter = (props) => {
+    const [count, setCount] = useState(0)
+    const countIncrementer = () => {
+        return setCount(count + 1)
+    }
+
     return(
         <div>
-            <h1></h1>
-            <button></button>
+            <h2>{props.count}</h2>
+            <button onClick={countIncrementer}>Increment the counter</button>
         </div>
     )
 };
