64daf7b25ad53b3e5bdb23ca4f6b4dbd029172c0
kit-trowbridge-mk <kittrow.dev@gmail.com>
2026-06-05 10:51:51 +0100

    standard form working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
+
 const Form = () => {
+    const [username, setUsername] = useState("")
+
+    const handleChange = (event) => {
+        const inputEl = event.target;
+        setUsername(inputEl.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch("http://url.com/endpoint", {
+            method: "POST",
+            body: JSON.stringify({username: username})
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Enter your username:
-                <input type="username" name="username"/>
+                <input 
+                    type="username" 
+                    name="username" 
+                    value={username}
+                    onChange={handleChange}
+                />
             </label>
+            <br />
+            <input 
+                type="submit" 
+                name="submit"
+            />
         </form>
     )
 }
