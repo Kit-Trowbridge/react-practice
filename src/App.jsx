7dfab1c4118f7008d501ba8f5c9663db9c87190a
kit-trowbridge-mk <kittrow.dev@gmail.com>
2026-06-05 10:19:22 +0100

    form setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./components/Product";
 import TypeLogger from "./components/TypeLogger";
 import ClickListener from "./components/ClickListener";
 import Counter from "./components/Counter";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
@@ -10,17 +11,8 @@ function App() {
     <>
       {/* <TypeLogger /> <br />
       <ClickListener /> */}
-      {/* <Profile 
-        name="Quackie Makers"
-        job="Makers' favourite rubber duck"
-        birthdate="2013"
-      /> */}
-      {/* <Product 
-        name="Air Fryer K2000"
-        description="The best air fryer to fry all things, even Mars bars"
-        price={2000}
-      /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <Form />
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+const Form = () => {
+    return (
+        <form>
+            <label>
+                Enter your username:
+                <input type="username" name="username"/>
+            </label>
+        </form>
+    )
+}
+
+export default Form
